b0df8ae2068468c25cfc37cd24fa5ff7d014a9cf
Danny Brandt <dvbrandt90@gmail.com>
2020-12-16 19:06:19 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -27,7 +27,9 @@ def second_challenge
        :age => "51"
       },
       :heroine => {
-       
+        :name => "Juliet",
+        :age => "15",
+        :status => "alive"
       },
       :heroine_friends => [
 
